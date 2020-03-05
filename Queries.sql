e018dfa951b35d4803985524858f02ea48670ebd
benjaminsmethurst64 <55767726+benjaminsmethurst64@users.noreply.github.com>
2020-03-04 19:50:19 -0600

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -2,8 +2,22 @@
 --Begin queries here
 
 select * from grads;
-
 select * from salary;
+
+select starting_median, salary.major
+from salary
+order by 
+starting_median;
+
+select mid_sal_upper, salary.major
+from salary
+order by 
+mid_sal_upper;
+
+select employed, total_grads, major
+from grads
+order by
+total_grads;
 
 SELECT grads.major, grads.total_grads, grads.employed, salary.starting_median, salary.mid_sal_median, salary.mid_sal_lower,salary.mid_sal_upper
 FROM grads
